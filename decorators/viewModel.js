79f5af3d82c4438517b60bfc8c709193821b6b19
Kuitos <kuitos.lau@gmail.com>
2017-08-04 14:59:07 +0800

    view model decorator

---
+++ decorators/viewModel.js
@@ -0,0 +1,18 @@
+/**
+ * @author Kuitos
+ * @homepage https://github.com/kuitos/
+ * @since 2017-08-04
+ */
+
+import invoker from '../core/dependency-inject/invoker';
+
+export default ViewModel => (target, name, descriptor) => {
+
+	if (name === void 0) {
+		throw new SyntaxError('you can\'t decorate a class with viewModel decorator');
+	}
+
+	const instance = invoker(InjectedClass, { scope: 'prototype' });
+	descriptor.initializer = () => instance;
+	descriptor.value = instance;
+};
