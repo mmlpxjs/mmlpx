26849e9d8abf2768ddc2a7e00dfab87ff5e100d5
Kuitos <kuitos.lau@gmail.com>
2018-01-29 16:33:03 +0800

    :bulb: warning when use store as a non-singleton mode

--- src/core/dependency-inject/initializers/store.ts
+++ src/core/dependency-inject/initializers/store.ts
@@ -15,6 +15,9 @@ export default function initialize<T>(this: any, injector: Injector, Store: any,
 	// if the first argument is a function, we can initialize it with the invoker instance `this`
 	if (isFunction(args[0])) {
 		constructorParams = flatten([args[0].call(this)]);
+		if (process.env.NODE_ENV !== 'production') {
+			console.warn('It looks like you are use Store with a non-singleton mode, which was deprecated!!', Store.name);
+		}
 	}
 
 	const name = Store[modelNameSymbol];
