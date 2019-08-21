0669dd3e192ed52b225e005074aa25d7f78c197a
anthhub <anthhub@163.com>
2019-08-21 11:40:53 +0800

    fix: remove log

--- src/life/init.ts
+++ src/life/init.ts
@@ -12,17 +12,13 @@ const sleep = (delay: number = 0) => {
 
 export async function init(options: any = {}) {
 	const { persist = true, StorageLoader: StorageLoader2 } = options;
-	console.log('%c%s', 'color: #20bd08;font-size:15px', '===TQY===: init -> StorageLoader2', StorageLoader2.key == StorageLoader.key);
 
 	if (persist) {
 		const res = StorageLoader.getSnapshot();
-		console.log('%c%s', 'color: #20bd08;font-size:15px', '===TQY===: init initinitinitinitinit -> StorageLoader', StorageLoader.snapshotKey);
-		console.log('%c%s', 'color: #20bd08;font-size:15px', '===TQY===: init -> res', res);
 		if (res) {
 			applySnapshot(res);
 		}
 		await sleep();
-		console.log('%c%s', 'color: #20bd08;font-size:15px', '===TQY===: init -> sleep');
 
 		persistence();
 	}
