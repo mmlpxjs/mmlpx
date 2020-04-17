a07cc392a8875016626c7fc9e1130bb4160a39d7
Misael dos Santos <47726159+misaeldossantos@users.noreply.github.com>
2020-04-17 13:04:34 -0300

    support for lazy injection
    if the injection decorator receives a function, the store will be resolved when requested, after its creation (class). This will allow mutual references - one store references another, and this reference is the first

--- src/core/dependency-inject/decorators/inject.ts
+++ src/core/dependency-inject/decorators/inject.ts
@@ -3,43 +3,50 @@
  * @homepage https://github.com/kuitos/
  * @since 2017-07-11
  */
+import "reflect-metadata";
+import hydrate from "../hydrate";
+import instantiate from "../instantiate";
+import { Constructor } from "../meta";
 
-import 'reflect-metadata';
-import hydrate from '../hydrate';
-import instantiate from '../instantiate';
-import { Constructor } from '../meta';
+export default <T>(
+  InjectedClass?: Constructor<T> | (() => Constructor<T>),
+  ...args: any[]
+): any => (target: any, property: string) => {
+  const symbol = Symbol(property);
 
-export default <T>(InjectedClass?: Constructor<T>, ...args: any[]): any => (target: any, property: string) => {
+  if (!InjectedClass) {
+    InjectedClass = Reflect.getMetadata("design:type", target, property);
+    /* istanbul ignore next */
+    if (!InjectedClass) {
+      throw new SyntaxError(
+        "You must pass a Class for injection while you are not using typescript!" +
+          'Or you may need to add "emitDecoratorMetadata: true" configuration to your tsconfig.json'
+      );
+    }
+  }
 
-	const symbol = Symbol(property);
+  return {
+    enumerable: true,
+    configurable: true,
+    get(this: any) {
+      if (typeof InjectedClass === "function") {
+        InjectedClass = InjectedClass();
+      }
 
-	if (!InjectedClass) {
-		InjectedClass = Reflect.getMetadata('design:type', target, property);
-		/* istanbul ignore next */
-		if (!InjectedClass) {
-			throw new SyntaxError('You must pass a Class for injection while you are not using typescript!' +
-				'Or you may need to add "emitDecoratorMetadata: true" configuration to your tsconfig.json');
-		}
-	}
-
-	return {
-		enumerable: true,
-		configurable: true,
-		get(this: any) {
-
-			if (!this[symbol]) {
-
-				const initializedValue = instantiate.apply(this, [InjectedClass, ...args]);
-				this[symbol] = initializedValue;
-				return initializedValue;
-
-			} else {
-				return hydrate(this[symbol], InjectedClass!, ...args);
-			}
-		},
-		// @formatter:off
-		// tslint:disable-next-line
-		set() {},
-		// @formatter:on
-	};
+      if (!this[symbol]) {
+        const initializedValue = instantiate.apply(this, [
+          InjectedClass,
+          ...args
+        ]);
+        this[symbol] = initializedValue;
+        return initializedValue;
+      } else {
+        return hydrate(this[symbol], InjectedClass!, ...args);
+      }
+    },
+    // @formatter:off
+    // tslint:disable-next-line
+    set() {}
+    // @formatter:on
+  };
 };
